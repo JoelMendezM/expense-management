34acf8396ee3a1ada561100fedb9b986afd55c83
Joel Mendez <joel.j.mendez.m@gmail.com>
2022-06-13 23:38:57 -0300

    logic for ingress and consume added, date input added

--- src/components/dataEntry/FormDataEntry.js
+++ src/components/dataEntry/FormDataEntry.js
@@ -3,44 +3,64 @@ import { v4 as uuid } from 'uuid';
 
 export const FormDataEntry = () => {
   const [data, setData] = useState([]);
-  const [balance, setBalance] = useState(0)
+  const [balance, setBalance] = useState(0);
+  const [entry, setEntry] = useState('')
   
   const handleSubmit = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     const inputData =
       {
       description: event.target.description.value,
       amount: event.target.amount.value,
-      balance: setBalance(balance + parseInt(event.target.amount.value))
+      date: event.target.date.value
     }
 
+    if (entry === 'ing') {
+      const ingress = {
+        ...inputData,
+        balance: setBalance(balance + parseInt(event.target.amount.value))
+      }
+      setData([...data, ingress])
+    } else {
+      const consume = {
+        ...inputData,
+        balance: setBalance(balance - parseInt(event.target.amount.value))
+      }
+      setData([...data, consume])
+    }
 
-
-    setData([...data, inputData])
     document.querySelector('#description').value = ''
     document.querySelector('#amount').value = ''
+    setEntry('') 
   }
 
   useEffect (() => {
   }, [data, balance])
   
-
   console.log('data', data);
 
   return (
     <div>
       <h1 className='flex justify-center font-bold'>Data Entry</h1>
       <form onSubmit={handleSubmit} className='m-3'>
-        <div className="inline-block relative w-64">
-          <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-3">
-            <option>Select entry type</option>
-            <option>Ingress</option>
-            <option>Cosume</option>
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        {
+        <div>
+          <div className="inline-block relative w-64">
+            <select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-3" 
+                    required
+                    onChange={(e) => setEntry(e.target.value)}
+                    value={entry}>
+              <option value='' disabled defaultValue hidden>Select entry type</option>
+              <option value='ing'>Ingress</option>
+              <option value='con'>Cosume</option>
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
           </div>
+          <input type='date' name='date' required/>
         </div>
+        }
         <div className="mb-6">
           <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Description</label>
           <input 
@@ -61,7 +81,7 @@ export const FormDataEntry = () => {
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
-      { data.length > 0 &&
+      {data.length > 0 &&
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -78,7 +98,7 @@ export const FormDataEntry = () => {
                         <th scope="col" className="px-6 py-3">
                         </th>
                         <th scope="col" className="px-6 py-3">
-                            <span className="sr-only">Edit</span>
+                          <span className="sr-only">Edit</span>
                         </th>
                     </tr>
                 </thead>
@@ -95,10 +115,10 @@ export const FormDataEntry = () => {
                                 {elem.amount}
                             </td>
                             <td className="px-6 py-4">
-                                Date
+                                {elem.date}
                             </td>
                             <td className="px-6 py-4 text-right">
-                                <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                <p className="font-medium text-blue-600 dark:text-blue-500 cursor-pointer">Remove</p>
                             </td>
                           </tr>
                         </>
